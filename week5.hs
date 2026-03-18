206c52a6cca483bed7d19691451d9d55bc7f966c
Sanvith Murari <sanvithmurari@gmail.com>
2026-03-18 21:31:39 +0530

    User defined data type codes

---
+++ week5.hs
@@ -0,0 +1,23 @@
+import Data.Text.Internal.Builder.Int.Digits (digits)
+--USER DEFINED DATA TYPES - WEEK 5
+--UDT 1
+data Day= Mon | Tue | Wed | Thu | Fri | Sat | Sun
+  deriving (Ord,Eq,Show,Enum)
+
+weekend :: Day -> Bool
+weekend d = d==Fri || d==Sat || d==Sun
+
+--UDT 2
+data Shape = Circle Double |Rectangle Double Double|Square Double|Triangle Double Double
+  deriving (Eq,Show)
+
+area:: Shape -> Double
+area (Circle r)=3.14*r*r
+area (Rectangle l b)= l*b
+area (Square s)=s*s
+area (Triangle b h)= 0.5*b*h
+
+instance Ord Shape where
+  (<=) :: Shape -> Shape -> Bool
+  s1<=s2 = area s1 <= area s2 
+
