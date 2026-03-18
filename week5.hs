fa819b62b0b9e9b954b883b01325042346652f72
Sanvith Murari <sanvithmurari@gmail.com>
2026-03-18 21:33:16 +0530

    Added digits extract and back to int

--- week5.hs
+++ week5.hs
@@ -21,3 +21,23 @@ instance Ord Shape where
   (<=) :: Shape -> Shape -> Bool
   s1<=s2 = area s1 <= area s2 
 
+--Random code to extract digits of a number and back to int
+extract ::Int -> [Int]
+extract 0=[]
+extract n = extract (div n 10) ++ [mod n 10]
+-- *OR using foldr
+digitsList :: Int -> [Int]
+digitsList n = foldr (\x acc -> acc ++ [x]) [] (go n) where
+  go 0 = []
+  go m = mod m 10 : go (div m 10)
+
+backtoint1 :: [Int] -> Int
+backtoint1 [] = 0
+backtoint1 (x:xs) =foldl (\y acc -> y*10 + acc) 0 (x:xs)
+
+backtoint2 :: [Int] -> Int
+backtoint2 [] = 0 
+backtoint2 (x:xs) = go (reverse (x:xs)) where
+  go [] = 0
+  go (y:ys) = y + 10 * go ys
+
