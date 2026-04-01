8ebadd268cc744a8abf769ca9be67838beeb4d2e
Sanvith Murari <sanvithmurari@gmail.com>
2026-04-01 20:56:24 +0530

    Recursion in Main to print many times

--- do.hs
+++ do.hs
@@ -15,5 +15,16 @@ main3=do
     putStr "Enter your Name: \n"
     name <- getLine
     print ("Name= " ++ name)
+
+--Recursion in a do block
+mainRec=do
+    putStr "Enter line to be printed 5 times using Recursion"
+    input <- getLine
+    printFive 5 input
+    where
+        printFive 1 str= putStrLn str
+        printFive n str = do
+            putStrLn str
+            printFive (n-1) str
     
 
