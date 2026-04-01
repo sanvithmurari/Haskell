74d1fc3e85d7f7fdbc8ae7950e115147861e7a66
Sanvith Murari <sanvithmurari@gmail.com>
2026-04-01 21:16:39 +0530

    Repeating an IO action n times

--- do.hs
+++ do.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 main1 = let {
     fact n
     |n==1=1
@@ -22,9 +23,27 @@ mainRec=do
     input <- getLine
     printFive 5 input
     where
-        printFive 1 str= putStrLn str
+        printFive 1 str= putStrLn str --OR printFive 0 str = return ()
         printFive n str = do
             putStrLn str
             printFive (n-1) str
-    
+
+--Repeat an IO action n times 
+nTimes :: Int -> IO () -> IO()
+nTimes 0 a = return ()
+nTimes n a = do
+    a
+    nTimes (n-1) a
+
+main4= nTimes 3 do{
+    putStr "Enter a Line: ";
+    str <- getLine;
+    putStrLn str;
+}
+
+main5 = do{
+    putStrLn "Enter a Line: ";
+    line <- getLine;
+    nTimes 5 (putStrLn line);
+}
 
